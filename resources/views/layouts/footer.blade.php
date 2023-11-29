1c070aaedf53c83be82e756386e48903ad3703ba
somantriagoes <somantriagoes@gmail.com>
2023-11-29 11:37:44 +0700

    add stisla tempate to project

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<div class="footer-left">
+    Copyright &copy; 2023 <div class="bullet"></div> Design By <a href="#">Agus Somantri</a>
+  </div>
+  <div class="footer-right">
+    1.0.0
+</div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('dashboard.home');
+});
+
+Route::get('/test', function () {
+    return view('dashboard.test');
+});
